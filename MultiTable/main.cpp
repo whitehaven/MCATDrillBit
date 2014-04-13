4b090325a69e2700a14f8ba9ea424d110270f336
whitehaven <accrist@me.com>
2014-04-12 21:35:07 -0600

    Initial commit.

--- MultiTable/main.cpp
+++ MultiTable/main.cpp
@@ -8,11 +8,11 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
     return 0;
 }
 
